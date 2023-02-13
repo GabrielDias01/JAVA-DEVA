e6351ebec857106535b6c546cf489068af4d5501
GabrielDias01 <gabrieldiassr@gmail.com>
2023-02-13 21:56:50 -0200

    13/02/2023

--- CalculadoraDescontos/App.java
+++ CalculadoraDescontos/App.java
@@ -2,6 +2,8 @@ package CalculadoraDescontos;
 
 public class App {
     public static void main(String[] args) {
-        
+        CalculadoraDescontos Objeto1 = new CalculadoraDescontos();
+        Objeto1.calcular20();
+        Objeto1.calcular30();
     }
 }
